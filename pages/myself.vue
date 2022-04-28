65f827d14a961b3b94e078b59b330cc18bc97fdd
brianmhlanga <brianmhlanga9@gmail.com>
2022-04-28 16:18:24 +0200

    Update myself.vue

--- pages/myself.vue
+++ pages/myself.vue
@@ -5,7 +5,7 @@
 <section class="w3l-about-breadcrumb text-center">
     <div class="breadcrumb-bg breadcrumb-bg-about py-sm-5 py-4">
         <div class="container py-2">
-            <h2 class="title">About Me</h2>
+            <h2 class="title">About Brian</h2>
             <ul class="breadcrumbs-custom-path mt-2">
                 <li><router-link to="/">Home</router-link></li>
                 <li class="active"><span class="fa fa-arrow-right mx-2" aria-hidden="true"></span> About Me </li>
